464ca14a9e250f18cec88d4076a27f7af5eaf5e3
Peruna-Solanum <gghernandez2122@gmail.com>
2026-06-26 10:45:07 -0400

    rewrote tradetracker.js to flow a bit better

--- footballtracker.js
+++ footballtracker.js
@@ -1,64 +1,21 @@
-//////////////////////////////////
-    Chart.register(ChartDataLabels);
-    Chart.register(window['chartjs-plugin-annotation']);
-///////////////////////////////////////////////////////
-/////////////////////////////////d/
-async function populate() {
+Chart.register(ChartDataLabels);
+Chart.register(window['chartjs-plugin-annotation']);
+
+async function loadJson() {
     const request = new Request('football2024.json');
-    
     const response = await fetch(request);
     const stats = await response.json();
-    file(stats);
-    mainPage(stats)
-    //chartExample(stats);
-
-    /////////////////////////////////////////////////////////////
-    function file (obj) {
+    fileLabelNamer(stats);
+    loadMainPage(stats);
+}
+loadJson();
+//populate()//call everything
+function fileLabelNamer(data) {
     const flLbl = document.getElementById('fileLbl');
-    const fileName = obj.year;
+    const fileName = data.year;
     flLbl.textContent = "File: " + fileName;
-    }
-////////////////////////////////////////////////////////////////////////
-function mainPage(obj) {
-    const names = [
-        obj.person[0].name, obj.person[1].name, obj.person[2].name, obj.person[3].name, obj.person[4].name, obj.person[5].name, 
-        obj.person[6].name, obj.person[7].name, obj.person[8].name
-    ];
-    let points = [//gets the points of each person, p1-p9. I figured out that there was no reason to const 500 things
-        obj.person[0].points, obj.person[1].points, obj.person[2].points, obj.person[3].points, obj.person[4].points, 
-        obj.person[5].points, obj.person[6].points, obj.person[7].points, obj.person[8].points, 
-    ]
-        let avgPoints = Math.round(
-            (points.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue
-                }, 0)
-            )/9);//the 0 is the initial value. This allows u to reduce whole array into a single # with your own function of course. 
-        // can label it any way u want. I then divided it by 9 and rounded it
-    let right = [
-        obj.person[0].totalRight, obj.person[1].totalRight, obj.person[2].totalRight, obj.person[3].totalRight, obj.person[4].totalRight, 
-        obj.person[5].totalRight, obj.person[6].totalRight, obj.person[7].totalRight, obj.person[8].totalRight, 
-    ]
-        let avgRight = Math.round(
-            (right.reduce((acc, currVal) => {
-                return acc + currVal
-            }, 0)
-            )/9);
-
-    let WWData = [Math.round(100*(right[0] / 285)), Math.round(100*(right[1] / 285)), Math.round(100*(right[2] / 285)), Math.round(100*(right[3] / 285)), 
-    Math.round(100*(right[4] / 285)), Math.round(100*(right[5] / 285)), Math.round(100*(right[6] / 285)), Math.round(100*(right[7] / 285)), Math.round(100*(right[8] / 285)), ];
-        let WWAvg = Math.round(
-            (WWData.reduce((accumulator, currentValue) => {
-            return accumulator + currentValue
-                }, 0)
-            )/9); 
- 
-    const totalPointsChart = document.getElementById('totalPoints').getContext('2d');
-    const totalRightChart = document.getElementById('totalRight').getContext('2d');
-    const WW = document.getElementById('WW').getContext('2d');
-
-Chart.defaults.color = 'white';
-Chart.defaults.borderColor = 'rgba(180, 151, 108, 0.72)';
-    function makeChart(context, title ,names, data, avg, zero, low, high, chartName) {
+}
+function makeChart(context, title ,names, data, avg, zero, low, high, chartName) {
          chartName = new Chart(context, {
         plugins: [ChartDataLabels, window['chartjs-plugin-annotation']],
         type: 'bar', // The type of chart we want to create
@@ -72,11 +29,9 @@ Chart.defaults.borderColor = 'rgba(180, 151, 108, 0.72)';
                 data: [data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8]], // Data points for the chart
                 backgroundColor: [
                     'rgba(223, 155, 54, 0.72)',
-
                 ],
                 borderColor: [
                     'rgba(143, 98, 73, 1)',
-
                 ],
                 borderWidth: 3
             }]
@@ -111,10 +66,38 @@ Chart.defaults.borderColor = 'rgba(180, 151, 108, 0.72)';
             }
         }
     });
-    }
+}
+function loadMainPage(data) {
+    const names = [data.person[0].name,data.person[1].name,data.person[2].name,data.person[3].name,
+        data.person[4].name,data.person[5].name,data.person[6].name,data.person[7].name,data.person[8].name];
+    const points = [data.person[0].points, data.person[1].points, data.person[2].points, data.person[3].points, data.person[4].points, 
+         data.person[5].points, data.person[6].points, data.person[7].points, data.person[8].points];
+    const right = [data.person[0].totalRight, data.person[1].totalRight, data.person[2].totalRight, data.person[3].totalRight, data.person[4].totalRight, 
+        data.person[5].totalRight, data.person[6].totalRight, data.person[7].totalRight, data.person[8].totalRight];
+    const WWData = [Math.round(100*(right[0] / 285)), Math.round(100*(right[1] / 285)), Math.round(100*(right[2] / 285)), Math.round(100*(right[3] / 285)), 
+    Math.round(100*(right[4] / 285)), Math.round(100*(right[5] / 285)), Math.round(100*(right[6] / 285)), Math.round(100*(right[7] / 285)), Math.round(100*(right[8] / 285))];
+
+    const avgPoints = Math.round(
+            (points.reduce((accumulator, currentValue) => {
+                return accumulator + currentValue}, 0)
+            )/9);
+    const avgRight = Math.round(
+            (right.reduce((acc, currVal) => {
+                return acc + currVal}, 0)
+            )/9); 
+    const WWAvg = Math.round(
+            (WWData.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue}, 0)
+            )/9); 
+ 
+    const totalPointsChart = document.getElementById('totalPoints').getContext('2d');//get context tells chart.js which chart goes where
+    const totalRightChart = document.getElementById('totalRight').getContext('2d');
+    const WW = document.getElementById('WW').getContext('2d');
+
+    Chart.defaults.color = 'white';
+    Chart.defaults.borderColor = 'rgba(180, 151, 108, 0.72)';
+    
     makeChart(totalPointsChart, "Total Points", names, points, avgPoints,false, 150);//technically have chartName off in function but it's not needed
     makeChart(totalRightChart, 'Total Games Correct', names, right, avgRight, false, 150);
     makeChart(WW, "Win %", names, WWData, WWAvg, true, 0, 100);
-}}
-
-populate()//call everything
+}
